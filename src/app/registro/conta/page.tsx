a306d2855ee196c01e0e57a551dd4cedc4353121
juankalleo <juankalleo4@gmail.com>
2026-01-16 03:56:37 -0300

    fix

--- src/app/registro/conta/page.tsx
+++ src/app/registro/conta/page.tsx
@@ -1,5 +1,10 @@
+import React, { Suspense } from 'react'
 import RegistroContaClient from './RegistroContaClient';
 
 export default function RegistroContaPage() {
-  return <RegistroContaClient />;
+  return (
+    <Suspense fallback={<div />}> 
+      <RegistroContaClient />
+    </Suspense>
+  );
 }
